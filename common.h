c1068da1daac7ee7be0bb6302ca86638fa07e8ad
dkaminski077 <dawidkaminski077@gmail.com>
2025-12-08 14:02:46 +0100

    Dodanie pliku common.h z definicjami struktur danych i zasobów IPC
    Plik zawiera definicje struktur (Dron, StanRoju), unię dla semaforów oraz stałe sterujące symulacją. Jest to plik nagłówkowy współdzielony.

---
+++ common.h
@@ -0,0 +1,64 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+#include <time.h>
+
+#define N 10
+#define POJEMNOSC_MAX 20
+#define POJEMNOSC_BAZY 4
+#define BAT_CRITICAL 20
+#define MAX_CYKLI 5
+
+#define FTOK_PATH "common.h"
+#define FTOK_ID 'R'
+
+//---STANY DRONA---
+#define STAN_WOLNY 0      //Dron nie istnieje
+#define STAN_LADOWANIE 1  //Dron jest w bazie i ładuje baterię
+#define STAN_LOT 2        //Dron lata poza bazą
+#define STAN_POWROT 3     //Dron czeka przed wejśceim do bazy
+#define STAN_ATAK 4       //Przeprowadza atak samobójczy
+#define STAN_ZNISZCZONY 5 //Bateria padła lub atak samobójczy
+
+struct Dron {
+    pid_t pid;
+    int bateria;
+    int liczba_cykli;
+    int stan;
+    int id_wewnetrzne;
+};
+
+struct StanRoju {
+    struct Dron drony[N * 2];
+    int pojemnosc_bazy;
+};
+
+#define TYP_DODAJ_PLATFORMY 1
+#define TYP_USUN_PLATFORMY 2
+
+struct Komunikat {
+    long mtype;
+    int dane;
+};
+
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
+
+#define SEM_BAZA 0
+#define SEM_WEJSCIE_1 1
+#define SEM_WEJSCIE_2 2
+#define SEM_PAMIEC 3
+#define ILOSC_SEMAFOROW 4
+
+#endif
