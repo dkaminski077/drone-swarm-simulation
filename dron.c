be33d40d623a4040ac5e33e87f068f9b3de1e845
dkaminski077 <dawidkaminski077@gmail.com>
2025-12-09 13:39:42 +0100

    Implementacja logiki symulacji: cykl życia drona i zarządzanie energią
    Zaimplementowano algorytm zachowania drona:
    1. Ładowanie i start z bazy.
    2. Lot i zużycie baterii.
    3. Procedura powrotu przy niskim stanie energii.
    4. Poprawna obsługa kolejki do bazy (zapobieganie zakleszczeniom).

--- dron.c
+++ dron.c
@@ -34,17 +34,109 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("   [DRON %d] PID: %d. Uruchomiony z pliku exec. Czekam 5s...\n", id_wew, getpid());
+    printf("   [DRON %d] PID: %d. Uruchomiony. Bateria 0%%.\n", id_wew, getpid());
 
-    sleep(5);
+    while(1) {
+        sleep(6);
 
-    printf("   [DRON %d] Koniec pracy. Zwalniam slot i bazę.\n", id_wew);
+        P(sem_id, SEM_PAMIEC);
+        roj->drony[id_wew].bateria = 100;
+        roj->drony[id_wew].liczba_cykli++;
+        int cykle = roj->drony[id_wew].liczba_cykli;
+        V(sem_id, SEM_PAMIEC);
+
+        if (cykle > MAX_CYKLI) {
+            printf("    [DRON %d] Zużyty (cykle: %d). Idę na złom.\n", id_wew, cykle);
+            break;
+        }
+
+        printf("    [DRON %d] Naładowany. Czekam na wylot.\n", id_wew);
+
+        int bramka = (rand()%2) + SEM_WEJSCIE_1;
+
+        P(sem_id, bramka);
+        printf("    [DRON %d] Wylatuję bramką %d...\n", id_wew, bramka - SEM_WEJSCIE_1 +1);
+        sleep(1);
+        V(sem_id, bramka);
+
+        V(sem_id, SEM_BAZA);
+
+        P(sem_id, SEM_PAMIEC);
+        roj->drony[id_wew].stan = STAN_LOT;
+        V(sem_id, SEM_PAMIEC);
+
+        printf("    [DRON %d] Wyleciałem. Latam...\n", id_wew);
+
+        while(1) {
+            sleep(1);
+
+            P(sem_id, SEM_PAMIEC);
+            roj->drony[id_wew].bateria -= 15;
+            int poziom = roj->drony[id_wew].bateria;
+            V(sem_id, SEM_PAMIEC);
+
+            if (poziom <= BAT_CRITICAL) {
+                printf("    [DRON %d] Bateria słaba (%d%%). Wracam do bazy.\n", id_wew, poziom);
+                break;
+            }
+
+            if (poziom <= 0) {
+                printf("    [DRON %d] Bateria 0%%. Rozbity.\n", id_wew);
+                P(sem_id, SEM_PAMIEC);
+                roj->drony[id_wew].stan = STAN_WOLNY;
+                V(sem_id, SEM_PAMIEC);
+                exit(0);
+            }
+        }
+
+        P(sem_id, SEM_PAMIEC);
+        roj->drony[id_wew].stan = STAN_POWROT;
+        V(sem_id, SEM_PAMIEC);
+
+        printf("    [DRON %d] Zbliżam się do bazy. Próbuję lądować...\n", id_wew);
+
+        while(1) {
+            struct sembuf wejdz = {SEM_BAZA, -1, IPC_NOWAIT};
+
+            if (semop(sem_id, &wejdz, 1) == 0) {
+                break;
+            } else {
+                P(sem_id, SEM_PAMIEC);
+                roj->drony[id_wew].bateria -= 1;
+                int poziom = roj->drony[id_wew].bateria;
+                V(sem_id, SEM_PAMIEC);
+
+                printf("    [DRON %d] Baza pełna. Krążę... (Bateria: %d%%)\n", id_wew, poziom);
+
+                if (poziom <=0 ) {
+                    printf("    [DRON %d] Bateria 0%%. Rozbity.\n", id_wew);
+                    P(sem_id, SEM_PAMIEC);
+                    roj->drony[id_wew].stan = STAN_WOLNY;
+                    V(sem_id, SEM_PAMIEC);
+                    exit(0);
+                }
+                sleep(1);
+            }
+        }
+
+        bramka = (rand()%2) + SEM_WEJSCIE_1;
+        P(sem_id, bramka);
+        printf("    [DRON %d] Ląduję bramką %d...\n", id_wew, bramka - SEM_WEJSCIE_1 + 1);
+        sleep(1);
+        V(sem_id, bramka);
+
+        P(sem_id, SEM_PAMIEC);
+        roj->drony[id_wew].stan = STAN_LADOWANIE;
+        V(sem_id, SEM_PAMIEC);
+    }
 
     P(sem_id, SEM_PAMIEC);
     roj->drony[id_wew].stan = STAN_WOLNY;
     V(sem_id, SEM_PAMIEC);
 
     V(sem_id, SEM_BAZA);
+
+    printf("    [DRON %d] Złomowanie zakończone.\n", id_wew);
 
     shmdt(roj);
     return 0;
