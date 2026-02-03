351b0067ef743c52ef5d6a67548eb839b097533c
dkaminski077 <dawidkaminski077@gmail.com>
2026-02-03 16:31:47 +0100

    (dowodca): poprawa stabilności interfejsu i walidacji celów
    - Dodano funkcję wyczysc_stdin, zapobiegającą zapętlaniu się menu po wprowadzeniu błędnych znaków.
    - Wprowadzono dodatkową walidację PID przed wysłaniem sygnału SIGUSR1, co eliminuje błędy przy próbie ataku na nieuruchomione jeszcze sloty.
    - Zoptymalizowano proces wysyłania komunikatów (flaga IPC_NOWAIT), zapobiegając blokowaniu interfejsu przy pełnej kolejce.

--- dowodca.c
+++ dowodca.c
@@ -1,132 +1,122 @@
 /*
  * Temat: Rój Dronów
  * Autor: Dawid Kamiński (155272)
- * 
- * Plik: dowodca.c (Interfejs Użytkownika)
- * 
- * Opis działania:
- * - Umożliwia interakcję użytkownika z systemem.
- * - Wysyła rozkazy (Rozbudowa/Redukcja) do Operatora za pomocą Kolejki Komuniktaów.
- * - Monitoruje stan roju czytając z Pamięci Dzielonej.
- * - Wysyła sygnał ataku (SIGUSR1) bezpośrednio do Dronów.
+ * * Plik: dowodca.c (Interfejs Taktyczny)
+ * * Opis działania:
+ * - Stanowi centrum sterowania systemem z perspektywy użytkownika.
+ * - Komunikuje się z Operatorem za pomocą Kolejek Komunikatów (Message Queues).
+ * - Pozwala na dynamiczne skalowanie populacji roju oraz bazy (Rozbudowa/Redukcja).
+ * - Implementuje mechanizm wskazywania celów do ataków samobójczych (sygnał SIGUSR1).
 */
 
 #include "common.h"
 
+/* * FUNKCJA: wyczysc_stdin
+ * Opis: Usuwa zbędne znaki z bufora wejściowego, zapobiegając błędnemu 
+ * przetwarzaniu menu w przypadku wprowadzenia nieprawidłowych danych.
+*/
+static void wyczysc_stdin(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
 int main() {
-    // Generator liczb losowych dla losowania celu ataku
+    // Inicjalizacja ziarna losowości unikalnego dla sesji dowódcy
     srand(time(NULL) ^ getpid());
 
-    // Generowanie kluczy IPC
+    // Pobieranie identyfikatorów zasobów IPC (Pamięć dzielona i Kolejka)
     key_t klucz_shm = ftok(FTOK_PATH, FTOK_ID);
-    key_t klucz_sem = ftok(FTOK_PATH, 'S');
     key_t klucz_msg = ftok(FTOK_PATH, 'Q');
 
-    // Uzyskanie dostępu do zasobów
     int shm_id = shmget(klucz_shm, sizeof(struct StanRoju), 0666);
-    int sem_id = semget(klucz_sem, ILOSC_SEMAFOROW, 0666);
     int msg_id = msgget(klucz_msg, 0666);
 
-    if (shm_id == -1 || sem_id == -1 || msg_id == -1) {
+    if (shm_id == -1 || msg_id == -1) {
         printf("BŁĄD: Nie mogę połączyć się z Rojem.\n");
         return 1;
     }
 
-    // Dołączenie pamięci dzielonej (tylko do odczytu)
+    // Dołączenie pamięci współdzielonej (odczyt stanu roju w czasie rzeczywistym)
     struct StanRoju *roj = (struct StanRoju*) shmat(shm_id, NULL, 0);
-
     if (roj == (void*) -1) {
         perror("Błąd shmat");
         return 1;
     }
 
-    char wybor;
-
-    // GŁOWNA PĘTLA INTERFEJSU
-    while(1) {
-        // Wyświetlanie menu i aktualnego stanu roju
-        printf("\n-----------------------------------\n");
-        printf("STATUS: Baza: %d miejsc | Limit dronów: %d/%d\n", roj->pojemnosc_bazy, 
-        roj->aktualny_limit_dronow, roj->max_limit_logiczny);
-        printf("\n-----------------------------------\n");
-        printf(" [1] ROZBUDOWA: Dodaj platformy.\n");
-        printf(" [2] REDUKCJA: Zdemontuj 50%% platfrom.\n");
-        printf(" [a] ATAK SAMOBÓJCZY: Wyślij sygnał zniszczenia.\n");
+    while (1) {
+        printf("\n--- DOWÓDCA ---\n");
+        printf(" [1] ROZBUDOWA (komunikat)\n");
+        printf(" [2] REDUKCJA  (komunikat)\n");
+        printf(" [a] ATAK SAMOBÓJCZY (SIGUSR1 do losowego drona)\n");
         printf(" [q] WYJŚCIE\n");
-        printf(" >");
+        printf(" > ");
 
-        scanf(" %c", &wybor);
+        char wybor;
+        if (scanf(" %c", &wybor) != 1) {
+            wyczysc_stdin();
+            continue;
+        }
 
-        // --- OPCJA 1: ROZBUDOWA (Komunikat do Operatora) ---
+        // --- OPCJA 1: ROZBUDOWA ROJU ---
         if (wybor == '1') {
-            struct Komunikat msg;
-            msg.mtype = TYP_DODAJ_PLATFORMY;
-            if(msgsnd(msg_id, &msg, sizeof(int), 0) == -1) {
-                perror("Błąd wysyłania komunikatu.\n");
+            struct Komunikat msg = { .mtype = TYP_DODAJ_PLATFORMY };
+            if (msgsnd(msg_id, &msg, sizeof(int), IPC_NOWAIT) == -1) {
+                perror("msgsnd rozbudowa");
             } else {
                 printf("[DOWÓDCA] Wysłano rozkaz rozbudowy.\n");
-                sleep(2);
             }
-        } 
-        // --- OPCJA 2: REDUKCJA (Komunikat do Operatora) ---
+        }
+        // --- OPCJA 2: REDUKCJA ROJU ---
         else if (wybor == '2') {
-            struct Komunikat msg;
-            msg.mtype = TYP_USUN_PLATFORMY;
-            if(msgsnd(msg_id, &msg, sizeof(int), 0) == -1) {
-                perror("Błąd wysyłania komunikatu.\n");
+            struct Komunikat msg = { .mtype = TYP_USUN_PLATFORMY };
+            if (msgsnd(msg_id, &msg, sizeof(int), IPC_NOWAIT) == -1) {
+                perror("msgsnd redukcja");
             } else {
                 printf("[DOWÓDCA] Wysłano rozkaz redukcji.\n");
-                sleep(2);
             }
-        } 
-        // --- OPCJA A: ATAK SAMOBÓJCZY (SYGNAŁ DO DRONA) ---
+        }
+        // --- OPCJA a: INICJACJA ATAKU ---
         else if (wybor == 'a' || wybor == 'A') {
-            printf("[DOWÓDCA] Szukam celu do ataku samobójczego...\n");
+            int kandydaci[MAX_DRONOW];
+            int licznik = 0;
 
-            /*
-             * Algorytm wyboru celu:
-             * 1. Skanowanie tablicy dronów w pamięci dzielonej.
-             * 2. Wyszukiwanie wszystkich aktywnych dronów (Lot/Powrót/Ładowanie).
-             * 3. Wylosowanie jednego drona z listy kandydatów.
-            */
-            int kadnydaci[MAX_DRONOW];
-            int licznik_kandydatow = 0;
-            for (int i=0; i<roj->max_limit_logiczny; i++) {
+            /* * ANALIZA STANU ROJU:
+             * Przeszukiwanie tablicy w poszukiwaniu dronów w stanach aktywnych.
+             */
+            for (int i = 0; i < roj->max_limit_logiczny; i++) {
                 int stan = roj->drony[i].stan;
-
-                if(stan == STAN_LOT || stan == STAN_POWROT || stan == STAN_LADOWANIE) {
-                    kadnydaci[licznik_kandydatow] = i;
-                    licznik_kandydatow++;
+                if (stan == STAN_LOT || stan == STAN_POWROT || stan == STAN_LADOWANIE) {
+                    kandydaci[licznik++] = i;
                 }
             }
 
-            if (licznik_kandydatow > 0) {
-                int wylosowany_index = rand() % licznik_kandydatow;
-                int cel_index = kadnydaci[wylosowany_index];
-
-                // Pobieranie danych celu
-                pid_t cel_pid = roj->drony[cel_index].pid;
-                int cel_id_wew = roj->drony[cel_index].id_wewnetrzne;
-                int cel_bateria = roj->drony[cel_index].bateria;
-                int cel_stan = roj->drony[cel_index].stan;
-
-                printf("[DOWÓDCA] Namierzono %d celów. Wylosowano drona nr %d.\n ATAK!\n", licznik_kandydatow, cel_id_wew);
-
-                // Wysłanie sygnału SIGUSR1 do procesu wylosowanwego drona
-                kill(cel_pid, SIGUSR1);
-
-                sleep(2);
+            if (licznik == 0) {
+                printf("[DOWÓDCA] Brak dostępnych celów.\n");
             } else {
-                printf("[DOWÓDCA] Brak dostępnych celów,\n");
+                // Losowy wybór celu spośród aktywnych jednostek
+                int idx = kandydaci[rand() % licznik];
+
+                pid_t pid = roj->drony[idx].pid;
+                int id_wew = roj->drony[idx].id_wewnetrzne;
+
+                if (pid <= 0) {
+                    printf("[DOWÓDCA] Wylosowano slot %d, ale PID niepoprawny (%d).\n", id_wew, pid);
+                } else {
+                    printf("[DOWÓDCA] ATAK -> dron %d (PID=%d)\n", id_wew, pid);
+                    // Przesłanie sygnału SIGUSR1 bezpośrednio do procesu drona
+                    if (kill(pid, SIGUSR1) == -1) {
+                        perror("kill(SIGUSR1)");
+                    }
+                }
             }
-        } 
-        // --- OPCJA Q: WYJŚCIE ---
+        }
+        // --- OPCJA q: WYJŚCIE ---
         else if (wybor == 'q' || wybor == 'Q') {
             break;
         }
     }
 
-    // Odłączenie pamięci dzielonej
+    // Odłączenie pamięci współdzielonej przed zakończeniem pracy interfejsu
     shmdt(roj);
     return 0;
 }
